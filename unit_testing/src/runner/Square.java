47e51fda8582a7b604fdb4311b244c3532189e47
govarthinisundararaj <35028923+govarthinisundararaj@users.noreply.github.com>
2018-04-24 11:46:36 +0000

    Update Square.java

--- unit_testing/src/runner/Square.java
+++ unit_testing/src/runner/Square.java
@@ -27,6 +27,30 @@ public class Square
 	{
 		return (e+f);
 	}
+	public int Squ_fun1(int x)
+	{
+	return x*x;
+	}
+	public int add_fun11(int a,int b)
+	{
+		return (a+b);
+	}
+	public int add_fun1111(int c,int d)
+	{
+		return (c+d);
+	}
+	public int add_fun21(int e,int f)
+	{
+		return (e+f);
+	}
+	public int add111(int e,int f)
+	{
+		return (e+f);
+	}
+	public int un2111(int e,int f)
+	{
+		return (e+f);
+	}
 	
 //Main program
 	public static void main(String[] args) 
